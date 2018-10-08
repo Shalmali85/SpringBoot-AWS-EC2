0d3ed454ee06d2fa62c30311f33d2f14cbeffeab
Shalmali85 <33997267+Shalmali85@users.noreply.github.com>
2018-10-08 16:28:52 +1100

    Add files via upload

--- feign-eureka-aws/client/src/main/java/demo/HelloClientApplication.java
+++ feign-eureka-aws/client/src/main/java/demo/HelloClientApplication.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-/**
- * @author Spencer Gibb
- */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
